54e165042d8fe3008c1d5734d036e09a9005a0ec
Nathan <137639586+SwissArmyWrench@users.noreply.github.com>
2024-10-07 10:37:35 -0400

    Finish get_today_handle() which fetches a file handle to the day's logging file, creating the file if it doesn't yet exist.

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,55 @@
 // Lib file for Workjournal
 
 use std::path::PathBuf;
-use chrono::prelude::*;
-
+// use chrono::prelude::*;
+use std::fs::OpenOptions;
+use std::fs::File;
 
 pub fn wip() {
-    get_file_handle();
+    let config: Config = Config { 
+        active_job: 49390,
+        logging_folder: PathBuf::from("/home/truepenny/logs_from_work"),
+        file_extension: Some(".txt")
+    };
+    let file = config.get_today_handle();
 }
 
 
 pub struct Config {
     active_job: u32,
     logging_folder: PathBuf,
-    file_extension: Option<String>,
+    file_extension: Option<&'static str>,
+}
+
+impl Config {
+fn get_today_handle(&self) -> std::fs::File {
+    // let logging_folder = PathBuf::from("/home/truepenny/logs_from_work");
+    let d_and_t = chrono::Local::now();
+    let date = d_and_t.date_naive();
+    let mut formatted: String = date.format("%Y-%m-%d").to_string();
+    formatted.push_str("-DL");
+    match self.file_extension {
+        Some(extension) => {formatted.push_str(extension)}, // Add file extension if specified
+        None => {} // do nothing
+    }
+    let mut fullpath = self.logging_folder.clone();
+    fullpath.push(formatted.clone());
+    println!("{}", fullpath.display());
+    match OpenOptions::new().append(true).open(fullpath.clone()) {
+        Ok(file) => { file },
+        Err(_) => {OpenOptions::new().create(true).append(true).open(fullpath).unwrap()}
+    }
+}
+
+    fn import_config() /* -> Config */ {
+        
+    }
 }
 
 pub struct Command {
     args: Vec<String>,
     // config:
-}
+} 
 
 impl Command {
     fn run(self) {
@@ -32,13 +63,4 @@ enum Intent {
 
 }
 
-fn get_file_handle() {
-    let logging_folder = PathBuf::from("/home/truepenny/logs_from_work");
-    let d_and_t = chrono::Local::now();
-    let date = d_and_t.date_naive();
-    let mut formatted = date.format("%Y-%m-%d").to_string();
-    formatted.push_str("-DL.txt");
-    let mut fullpath = logging_folder;
-    fullpath.push(formatted.clone());
-    println!("file: {}", fullpath.display());
-}
+
