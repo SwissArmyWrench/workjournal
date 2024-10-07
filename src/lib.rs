fa4615fd343d7fbc10d164d66e8de165843b5b08
Nathan <137639586+SwissArmyWrench@users.noreply.github.com>
2024-10-07 12:48:35 -0400

    Load a Config struct from a config file, using directories::ProjectDirs to handle the path to that file in a system-agnostic way and serde / serde_yaml for parsing the YAML content of the file

--- src/lib.rs
+++ src/lib.rs
@@ -2,57 +2,76 @@
 
 use std::path::PathBuf;
 // use chrono::prelude::*;
+use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::Write;
-use std::fs::File;
+use directories::ProjectDirs;
+use serde::{Serialize, Deserialize};
 
 pub fn wip() {
-    let config: Config = Config { 
+    /* let config: Config = Config {
         active_job: 49390,
         logging_folder: PathBuf::from("/home/truepenny/logs_from_work"),
-        file_extension: Some(".txt")
-    };
+        file_extension: Some(String::from(".txt")),
+    }; */
+
+    let config = Config::load().unwrap();
+    
+    /*
     let mut file = config.get_today_handle();
-    // std::fs::write(format!("The active job is {}", config.active_job).as_bytes(), file);
+    std::fs::write(format!("The active job is {}", config.active_job).as_bytes(), file);
     file.write(format!("The active job is {}\n", config.active_job).as_bytes());
+    */
+    println!("Active job: {}", config.active_job);
 }
 
-
+#[derive(Serialize, Deserialize)]
 pub struct Config {
     active_job: u32,
     logging_folder: PathBuf,
-    file_extension: Option<&'static str>,
+    file_extension: Option<String>,
 }
 
 impl Config {
-fn get_today_handle(&self) -> std::fs::File {
-    // let logging_folder = PathBuf::from("/home/truepenny/logs_from_work");
-    let d_and_t = chrono::Local::now();
-    let date = d_and_t.date_naive();
-    let mut formatted: String = date.format("%Y-%m-%d").to_string();
-    formatted.push_str("-DL");
-    match self.file_extension {
-        Some(extension) => {formatted.push_str(extension)}, // Add file extension if specified
-        None => {} // do nothing
+    fn get_today_handle(&self) -> std::fs::File {
+        // let logging_folder = PathBuf::from("/home/truepenny/logs_from_work");
+        let d_and_t = chrono::Local::now();
+        let date = d_and_t.date_naive();
+        let mut formatted: String = date.format("%Y-%m-%d").to_string();
+        formatted.push_str("-DL");
+        match &self.file_extension {
+            Some(extension) => {formatted = format!("{}{}", formatted, extension)} // .push_str(extension), // Add file extension if specified
+            None => {}                                        // do nothing
+        }
+        let mut fullpath = self.logging_folder.clone();
+        fullpath.push(formatted.clone());
+        println!("{}", fullpath.display());
+        match OpenOptions::new().append(true).open(fullpath.clone()) {
+            Ok(file) => file,
+            Err(_) => OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(fullpath)
+                .unwrap(),
+        }
     }
-    let mut fullpath = self.logging_folder.clone();
-    fullpath.push(formatted.clone());
-    println!("{}", fullpath.display());
-    match OpenOptions::new().append(true).open(fullpath.clone()) {
-        Ok(file) => { file },
-        Err(_) => {OpenOptions::new().create(true).append(true).open(fullpath).unwrap()}
-    }
-}
 
-    fn import_config() /* -> Config */ {
+    fn load() -> Result<Config, serde_yaml::Error> {
+        let dirs = ProjectDirs::from("com", "SwissArmyWrench", "Workjournal").unwrap(); // SAFE
+        let mut config_path = dirs.config_dir().to_owned();
+        config_path.push("config.yaml");
+        let reader = File::open(config_path).expect("The system was unable to open the config file");
+        let config: Result<Config, serde_yaml::Error> = serde_yaml::from_reader(reader);
+        config // needed to define a result to turn in order to give the type hint above
         
     }
 }
 
 pub struct Command {
     args: Vec<String>,
+    intent: Intent
     // config:
-} 
+}
 
 impl Command {
     fn run(self) {
@@ -63,7 +82,4 @@ impl Command {
 enum Intent {
     ChangeActive(u32),
     MakeNote(String),
-
 }
-
-
