f41aa37956b13c5722e91924d55476e4f28aaf86
Nathan <137639586+SwissArmyWrench@users.noreply.github.com>
2024-10-26 20:20:17 -0400

    Skeleton implementation of clap

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
-use std::env;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version = "1.1.0", about = "Keeping notes on your workday, easily, in the terminal")]
+struct CmdIn {
+    #[command(subcommand)]
+    command: Subcommands,
+    
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum Subcommands {
+    /// Makes a note under the active job
+    Mknote {
+        note: String,
+        #[arg(short = 'j', long = "job")]
+        job: Option<u32>
+    },
+    /// Changes the active job
+    Chactive {
+        jobnumber: u32
+    },
+    /// Prints the active order number
+    Active,
+    /// Prints the path where Workjournal looks for its config file
+    Configpath,
+    /// Prints the notes for a given job number
+    Print {
+        jobnumber: u32
+    }
+}
+
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    // workjournal::wip();
-
-    let mut intent: workjournal::Intent = workjournal::Intent::NoCmd;
-    if args.len() == 1 {
-        println!("Interactive mode not yet ready!")
-    } else if args[1] == "chactive" {
-        intent = workjournal::Intent::ChangeActive(args[2].parse::<u32>().unwrap());
-    } else if args[1] == "mknote" {
-        if args[2] == "-j" || args[2] == "--job" {
-            let note = env::args().skip(4).collect::<Vec<String>>().join(" ");
-            intent = workjournal::Intent::MakeNoteOnJob(note, args[3].parse::<u32>().unwrap());
-        } else {
-            let note = env::args().skip(2).collect::<Vec<String>>().join(" ");
-            intent = workjournal::Intent::MakeNote(note);
-        }
-    } else if args[1] == "print" {
-        intent = workjournal::Intent::PrintNotes(args[2].parse::<u32>().unwrap());
-    } else if args[1] == "active" {
-        intent = workjournal::Intent::GetCurrentJob;
-    } else if args[1] == "configpath" {
-        workjournal::configpath();
-        std::process::exit(0)
-    }
-
-    workjournal::Command::new(args, intent, workjournal::Config::load().unwrap()).run();
+    let cmd = CmdIn::parse()
 }
